6dabeb1aede9ece1c64a48b249a98812572ff942
Takashi Kaneda <takashi.kaneda@futurestandard.co.jp>
2019-12-23 00:43:56 +0900

    Add buffer

--- src/lib/apiGatewayCore/simpleHttpClient.js
+++ src/lib/apiGatewayCore/simpleHttpClient.js
@@ -12,7 +12,6 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-/* eslint max-len: ["error", 100]*/
 
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
